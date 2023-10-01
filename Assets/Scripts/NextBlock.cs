e3e9edf0ddc9af8bf40cd0d09363497946eaea10
Gamerize <gundam.issacwai@gmail.com>
2023-10-01 01:00:09 +0100

    Update NextBlock.cs

--- Assets/Scripts/NextBlock.cs
+++ Assets/Scripts/NextBlock.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class NextBlock : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] Blocks;
+    private GameObject[] m_Blocks;
+    [SerializeField]
+    GameObject[] m_BlockList = new GameObject[5];
+    private int m_RandomInt;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +23,16 @@ public class NextBlock : MonoBehaviour
         
     }
 
-    void RandomizeNextBlock()
+    void AddNextBlock()
     {
-
+        m_RandomInt = Random.Range(0,m_Blocks.Length+1);
+        for (int i = 0; i < m_BlockList.Length; i++)
+        {
+            if (m_BlockList[i] == null)
+            {
+                m_BlockList[i] = m_Blocks[m_RandomInt];
+            }
+        }
     }
 
 }
